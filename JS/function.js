90082033f0a2e3b6178a5f8ba0f431f4e42734e4
EmersonGGS <emersongstewart@gmail.com>
2014-03-02 10:59:02 -0500

    Parse update
    Parse being added

--- JS/function.js
+++ JS/function.js
@@ -13,12 +13,23 @@ window.onload=function(){
 		menuStatus = "closed";
 	});
 	$('#two').on("swipeleft", function(){
+
+	//Set last page signup values
+	document.getElementById("confirm-name").value = document.getElementById("signup-name").value;
+	document.getElementById("confirm-email").value = document.getElementById("signup-email").value;
+	document.getElementById("confirm-password").value = document.getElementById("signup-password").value;
 var nextpage = $(this).next('div[data-role="page"]');
 if (nextpage.length > 0) {
 $.mobile.changePage(nextpage, "slide", false, true);
 }
 });
 $('#three').on("swiperight", function(){
+
+	//Set last page signup values
+	document.getElementById("confirm-year").value = document.getElementById("signup-year").value;
+	document.getElementById("confirm-make").value = document.getElementById("signup-make").value;
+	document.getElementById("confirm-model").value = document.getElementById("signup-model").value;
+	document.getElementById("confirm-engine").value = document.getElementById("signup-engine").value;
 var prevpage = $(this).prev('div[data-role="page"]');
 if (prevpage.length > 0) {
 $.mobile.changePage(prevpage, {transition: "slide",
--- JS/parse_functions.js
+++ JS/parse_functions.js
@@ -3,17 +3,52 @@ Parse.initialize("v72tuCZdZwu7boW8XlUFOdfypGS52j1rSMw1YYX7",
 
 function createUser(){
 	var user = new Parse.User();
-	user.set("username", document.getElementById("signup-name").value);
-	user.set("email", document.getElementById("signup-email").value);
-	user.set("password", document.getElementById("signup-password").value);
+	user.set("username", document.getElementById("confirm-name").value);
+	user.set("email", document.getElementById("confirm-email").value);
+	user.set("password", document.getElementById("confirm-password").value);
+
+	var CarInfo = Parse.Object.extend("CarInfo");
+	var carInfo = new CarInfo();
+
+	carInfo.set("user", document.getElementById("confirm-email").value);
+	carInfo.set("carYear", document.getElementById("confirm-year").value);
+	carInfo.set("carMake", document.getElementById("confirm-make").value);
+	carInfo.set("carModel", document.getElementById("confirm-model").value);
+	carInfo.set("carEngine", document.getElementById("confirm-engine").value);
+
+
+
+	carInfo.save(null, {
+  success: function(carInfo) {
+    //if save succeeds
+    console.log('New object created with objectId: ' + carInfo.id);
+  },
+  error: function(carInfo, error) {
+    //if save fails
+    alert('Failed to create new object, with error code: ' + error.description);
+  }
+});
 
 	user.signUp(null, {
 	  success: function(user) {
-	    // Hooray! Let them use the app now.
+	    console.log("Account created!");
+	    location.href = "#one";
 	  },
 	  error: function(user, error) {
 	    // Show the error message somewhere and let the user try again.
 	    alert("Error: " + error.code + " " + error.message);
 	  }
 	});
+}
+
+function loginUser(){
+	Parse.User.logIn(document.getElementById("login-name").value, document.getElementById("login-password").value, {
+  success: function(user) {
+  	alert("Welcome back!");
+  	location.href = "#six"
+  },
+  error: function(user, error) {
+    alert("Invalid login information, give it another go!");
+  }
+});
 }
