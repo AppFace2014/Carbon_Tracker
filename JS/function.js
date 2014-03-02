9e1d8b882e89f52422059ffb1d7ff24e5f8af572
jamesm1984 <artur@Jamess-MacBook-Pro.local>
2014-03-02 11:19:32 -0500

    upload

--- JS/function.js
+++ JS/function.js
@@ -7,29 +7,49 @@ window.onload=function(){
 	});
 
 	jQuery('.pMenu').on("tap",function(){
-		jQuery('.pMenu').animate({
-			right: "-155px"
-        }, 300);
-		menuStatus = "closed";
-	});
-
-};
-/*window.pageload=function(){
-	
-};
-
-window.pagechange = function(){
-	jQuery('#profile').on("tap",function(){
 		changeMenu();
 	});
 
-	jQuery('#pMenu').on("tap",function(){
-		jQuery('#pMenu').animate({
-			right: "-155px"
-        }, 300);
-		menuStatus = "closed";
-	});
-}*/
+	$('#two').on("swipeleft", function(){
+
+//Set last page signup values
+document.getElementById("confirm-name").value = document.getElementById("signup-name").value;
+document.getElementById("confirm-email").value = document.getElementById("signup-email").value;
+document.getElementById("confirm-password").value = document.getElementById("signup-password").value;
+var nextpage = $(this).next('div[data-role="page"]');
+if (nextpage.length > 0) {
+$.mobile.changePage(nextpage, "slide", false, true);
+}
+});
+$('#three').on("swiperight", function(){
+
+//Set last page signup values
+document.getElementById("confirm-year").value = document.getElementById("signup-year").value;
+document.getElementById("confirm-make").value = document.getElementById("signup-make").value;
+document.getElementById("confirm-model").value = document.getElementById("signup-model").value;
+document.getElementById("confirm-engine").value = document.getElementById("signup-engine").value;
+var prevpage = $(this).prev('div[data-role="page"]');
+if (prevpage.length > 0) {
+$.mobile.changePage(prevpage, {transition: "slide",
+reverse: true}, true, true);
+}
+});
+$('#three').on("swipeleft", function(){
+var nextpage = $(this).next('div[data-role="page"]');
+if (nextpage.length > 0) {
+$.mobile.changePage(nextpage, "slide", false, true);
+}
+});
+
+$('#four').on("swiperight", function(){
+var prevpage = $(this).prev('div[data-role="page"]');
+if (prevpage.length > 0) {
+$.mobile.changePage(prevpage, {transition: "slide",
+reverse: true}, true, true);
+}
+});
+
+};
 
 function changeMenu(){
 	if (menuStatus != "open"){
@@ -46,3 +66,4 @@ function changeMenu(){
 		menuStatus = "closed";
 	}	
 }
+
