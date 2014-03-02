c5dcba76b3fe14d40310a550b1b6344c8a16e3f5
jamesm1984 <artur@Jamess-MacBook-Pro.local>
2014-03-02 12:22:28 -0500

    js revert

--- JS/function.js
+++ JS/function.js
@@ -1,24 +1,20 @@
 var menuStatus;
 
 window.onload=function(){
-	
-	jQuery('.profile').on("tap",function(){
-		changeMenu();
 
-	});
+jQuery('.profile').on("tap",function(){
+changeMenu();
+});
 
-	jQuery('.pMenu').on("tap",function(){
-		changeMenu();
-	});
-	$('#two').on("swipeleft", function(){
+jQuery('.pMenu').on("tap",function(){
+changeMenu();
+});
+$('#two').on("swipeleft", function(){
 
-
-	$('#two').on("swipeleft", function(){
-
-	//Set last page signup values
-	document.getElementById("confirm-name").value = document.getElementById("signup-name").value;
-	document.getElementById("confirm-email").value = document.getElementById("signup-email").value;
-	document.getElementById("confirm-password").value = document.getElementById("signup-password").value;
+//Set last page signup values
+document.getElementById("confirm-name").value = document.getElementById("signup-name").value;
+document.getElementById("confirm-email").value = document.getElementById("signup-email").value;
+document.getElementById("confirm-password").value = document.getElementById("signup-password").value;
 var nextpage = $(this).next('div[data-role="page"]');
 if (nextpage.length > 0) {
 $.mobile.changePage(nextpage, "slide", false, true);
@@ -26,24 +22,18 @@ $.mobile.changePage(nextpage, "slide", false, true);
 });
 $('#three').on("swiperight", function(){
 
-	//Set last page signup values
-	document.getElementById("confirm-year").value = document.getElementById("signup-year").value;
-	document.getElementById("confirm-make").value = document.getElementById("signup-make").value;
-	document.getElementById("confirm-model").value = document.getElementById("signup-model").value;
-	document.getElementById("confirm-engine").value = document.getElementById("signup-engine").value;
+//Set last page signup values
+document.getElementById("confirm-year").value = document.getElementById("signup-year").value;
+document.getElementById("confirm-make").value = document.getElementById("signup-make").value;
+document.getElementById("confirm-model").value = document.getElementById("signup-model").value;
+document.getElementById("confirm-engine").value = document.getElementById("signup-engine").value;
 var prevpage = $(this).prev('div[data-role="page"]');
 if (prevpage.length > 0) {
 $.mobile.changePage(prevpage, {transition: "slide",
 reverse: true}, true, true);
 }
 });
-	$('#three').on("swipeleft", function(){
-
-	//Set last page signup values
-	document.getElementById("confirm-year").value = document.getElementById("signup-year").value;
-	document.getElementById("confirm-make").value = document.getElementById("signup-make").value;
-	document.getElementById("confirm-model").value = document.getElementById("signup-model").value;
-	document.getElementById("confirm-engine").value = document.getElementById("signup-engine").value;
+$('#three').on("swipeleft", function(){
 var nextpage = $(this).next('div[data-role="page"]');
 if (nextpage.length > 0) {
 $.mobile.changePage(nextpage, "slide", false, true);
@@ -58,3 +48,20 @@ reverse: true}, true, true);
 }
 });
 };
+
+
+function changeMenu(){
+if (menuStatus != "open"){
+console.log("open");
+jQuery('.pMenu').animate({
+right: "5px"
+        }, 300);
+menuStatus = "open";
+} else {
+console.log("closed");
+jQuery('.pMenu').animate({
+right: "-155px"
+        }, 300);
+menuStatus = "closed";
+}	
+}
